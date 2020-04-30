0e278c6bb2eeccc08eeaf30b32ab141dc5e8c807
jean <jeangraff30@gmail.com>
2020-04-29 22:20:26 -0400

    sdm660-common: Drop snap package
    Co-authored-by: ronald-b <ronaldsantospetersen@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -166,7 +166,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
-    Snap
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
